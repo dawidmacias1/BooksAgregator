d490e23fac614b159bbc465d925ef672f3098efe
Dawid Macias <dawidmacias95@gmail.com>
2019-10-15 22:58:58 +0200

    Add spring boot project

---
+++ src/main/java/pl/booksagregator/BooksAgregatorApplication.java
@@ -0,0 +1,13 @@
+package pl.booksagregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksAgregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksAgregatorApplication.class, args);
+	}
+
+}
