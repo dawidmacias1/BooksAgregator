f5a290754b51f9d32edf52592e762af0086fe4e6
Dawid Macias <dawidmacias95@gmail.com>
2019-10-15 23:14:21 +0200

    Add test controller

---
+++ src/main/java/pl/booksagregator/FirstController.java
@@ -0,0 +1,16 @@
+package pl.booksagregator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class FirstController {
+
+    @GetMapping("{input}")
+    public String reverse(@PathVariable String input) {
+        return new StringBuilder(input).reverse().toString();
+    }
+}
