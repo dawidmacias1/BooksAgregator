efb80e503769944af9e826e7a8bb8c7ba274c50c
Rahly <209413@edu.p.lodz.pl>
2019-10-19 22:25:57 +0200

    Add user main panel layout with example navigation drawer whichwill be modified when we create mockup for this feature

--- app/src/main/java/com/example/booksagregator/LoginActivity.java
+++ app/src/main/java/com/example/booksagregator/LoginActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class LoginActivity extends AppCompatActivity {
 
     Button goToRegisterPanelButton;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +18,21 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login_panel);
 
         goToRegisterPanelButton = findViewById(R.id.button_go_to_register_activity);
+        loginButton = findViewById(R.id.button_login_confirm);
 
         goToRegisterPanelButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(intent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LoginActivity.this, UserMainPanel.class);
                 LoginActivity.this.startActivity(intent);
             }
         });
