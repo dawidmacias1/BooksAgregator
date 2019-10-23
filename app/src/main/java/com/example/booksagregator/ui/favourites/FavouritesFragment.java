4a26a64b7f2d4ef0d39035a487c63856e62b6939
Rahly <209413@edu.p.lodz.pl>
2019-10-23 17:38:07 +0200

    Modified user panel layout and added favourite fragment layout

---
+++ app/src/main/java/com/example/booksagregator/ui/favourites/FavouritesFragment.java
@@ -0,0 +1,90 @@
+package com.example.booksagregator.ui.favourites;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.booksagregator.R;
+import com.example.booksagregator.ResourceActivity;
+
+public class FavouritesFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             final ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_favourites, container, false);
+//        final TextView textView = root.findViewById(R.id.text_home);
+//        homeViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+
+
+        LinearLayout gallery = root.findViewById(R.id.gallery);
+        LinearLayout gallery2 = root.findViewById(R.id.gallery2);
+        LinearLayout gallery3 = root.findViewById(R.id.gallery3);
+
+
+        inflater = LayoutInflater.from(getActivity());
+
+
+        for (int i = 0; i < 6; i++) {
+
+            View view = inflater.inflate(R.layout.item, gallery, false);
+            View view2 = inflater.inflate(R.layout.item, gallery2, false);
+            View view3 = inflater.inflate(R.layout.item, gallery3, false);
+
+            ImageView imageView = view.findViewById(R.id.image);
+            imageView.setImageResource(R.drawable.ic_book_resource_24dp);
+            ImageView imageView2 = view2.findViewById(R.id.image);
+            imageView2.setImageResource(R.drawable.ic_book_resource_24dp);
+            ImageView imageView3 = view3.findViewById(R.id.image);
+            imageView3.setImageResource(R.drawable.ic_book_resource_24dp);
+
+            gallery.addView(view);
+            gallery2.addView(view2);
+            gallery3.addView(view3);
+        }
+
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent act2 = new Intent(container.getContext(), ResourceActivity.class);
+                startActivity(act2);
+            }
+        });
+
+        gallery2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent act2 = new Intent(container.getContext(), ResourceActivity.class);
+                startActivity(act2);
+            }
+        });
+
+        gallery3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent act2 = new Intent(container.getContext(), ResourceActivity.class);
+                startActivity(act2);
+            }
+        });
+
+        return root;
+    }
+
+
+}
