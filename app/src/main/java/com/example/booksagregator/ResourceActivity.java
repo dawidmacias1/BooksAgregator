ca6963342aeac192b86b64f49637363b3ad19a86
Rahly <209413@edu.p.lodz.pl>
2019-10-29 21:56:38 +0100

    Added rating bar controller

--- app/src/main/java/com/example/booksagregator/ResourceActivity.java
+++ app/src/main/java/com/example/booksagregator/ResourceActivity.java
@@ -1,14 +1,26 @@
 package com.example.booksagregator;
 
 import android.os.Bundle;
+import android.widget.RatingBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResourceActivity extends AppCompatActivity {
 
+    RatingBar rateItem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resource_panel_activity);
+
+        rateItem = findViewById(R.id.ratingBar);
+
+        rateItem.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+
+            }
+        });
     }
 }
